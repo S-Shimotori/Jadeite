a58b15bb60ae10d1685103fba0eaf8816b812951
Shigure Shimotori <S_Shimotori@Shigures-Mac-mini.local>
2015-01-11 17:03:41 +0900

    AppDelegateのほうをタブめにゅーをつかえるようにかいた。

--- Jadeite/AppDelegate.swift
+++ Jadeite/AppDelegate.swift
@@ -13,10 +13,14 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-
+    var navigationController: UINavigationController?
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        // Override point for customization after application launch.
+        let firstView:ViewController = ViewController()
+        navigationController = UINavigationController(rootViewController:firstView)
+        self.window = UIWindow(frame:UIScreen.mainScreen().bounds)
+        self.window?.rootViewController = navigationController
+        self.window?.makeKeyAndVisible()
         return true
     }
 
