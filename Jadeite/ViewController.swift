16ca559178d4f92dd51ea443df7f7d5879517d57
Shigure Shimotori <S_Shimotori@Shigures-Mac-mini.local>
2015-01-11 18:29:31 +0900

    画面の下の方にボタンがつきました。FontAwesomeつかいました。

--- Jadeite/ViewController.swift
+++ Jadeite/ViewController.swift
@@ -12,7 +12,19 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
+        self.navigationController?.setToolbarHidden(false,animated:false)
+        //下のところ
+        navigationController?.toolbar.tintColor = UIColor.blueColor()
+        //let toSubjectViewButton = UIBarButtonItem(title:"\u{E800}",style: UIBarButtonItemStyle.Bordered,target:self,action:"toSubjectView:")
+        let toSubjectViewButton = UIBarButtonItem()
+        toSubjectViewButton.title = "\u{f03a}"
+        toSubjectViewButton.setTitleTextAttributes([NSFontAttributeName:UIFont(name:"FontAwesome",size:20)!],forState:UIControlState.Normal)
+        //([NSFontAttributeName:UIFont(name:"fontawesome-webfont",size:15)],forState:UIControlState.Normal)
+        toSubjectViewButton.target = self
+        toSubjectViewButton.action = "toSubjectView:"
+        let flexibleSpace:UIBarButtonItem = UIBarButtonItem(barButtonSystemItem:UIBarButtonSystemItem.FlexibleSpace,target: nil,action:nil)
+        self.toolbarItems = NSArray(arrayLiteral:flexibleSpace,toSubjectViewButton)
     }
     
     override func didReceiveMemoryWarning() {
@@ -20,5 +32,9 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    
+    func toSubjectView(button:UIButton){
+        let nextView = SubjectView()
+        //SecondViewに移動する.
+        self.navigationController?.pushViewController(nextView,animated: true)
+    }
 }
